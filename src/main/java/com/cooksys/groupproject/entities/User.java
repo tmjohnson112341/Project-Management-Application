7303b32e8277dcd45ffe85f03a64c3fc4109ca59
Tina Johnson <tmjohnson112341@gmail.com>
2022-10-31 18:00:48 -0400

    seeded

--- src/main/java/com/cooksys/groupproject/entities/User.java
+++ src/main/java/com/cooksys/groupproject/entities/User.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name="user_table")
 public class User {
 	
 	@Id

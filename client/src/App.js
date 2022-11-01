bbe3f07d2901c977d2e63202dc8762fd6fb233f2
Brandon T <98200133+btomecsko@users.noreply.github.com>
2022-11-01 17:05:00 -0500

    made changes to match closer to branches

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 
 
-import NavBar from "./NavBar";
-import Login from "./Login";
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -11,6 +11,8 @@ function App() {
       <NavBar />
       <Routes>
           <Route path="/" element={<Login/>}/>
+          <Route path="/announcements" element={<Announcements />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   );

3e7671d42d41e2123d77bd7b9d55858f7f04e0e8
Brandon T <98200133+btomecsko@users.noreply.github.com>
2022-11-03 20:28:13 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from "./components/Login";
+import Announcements from "./components/Announcements";
+import ProjectContainer from "./components/ProjectContainer";
+import Project from "./components/Project";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Announcements />} />
+          <Route path="/projectlist" element={<ProjectContainer />} />
+          <Route path="/project" element={<Project />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;

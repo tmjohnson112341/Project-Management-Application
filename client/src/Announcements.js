0f5c9e0e606de2a727ec3b1da4290c731b70c5ad
Brandon T <98200133+btomecsko@users.noreply.github.com>
2022-11-03 20:33:08 -0500

    Update Announcements.js

---
+++ client/src/Announcements.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ACard from './ACard'
+import NavBar from './NavBar'
+import { useState, useEffect } from 'react'
+
+const Announcements = () => {
+  const [announcementData, setAnnouncementData] = useState({});
+
+  const companyID = JSON.parse(localStorage.getItem("companyID"))
+
+  const loadAnnouncements = async () => {
+    const response = await fetch(`http://localhost:8080/announcements/company/${companyID}`,{mode:"cors"});
+    let data = await response.json();
+    setAnnouncementData(data);
+  }
+
+  console.log(announcementData);
+  useEffect(() => {
+    loadAnnouncements();
+  }, []);
+
+
+  return (
+    <>
+    <NavBar />
+    <div className="main-container">
+      <h4 style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '48px', fontWeight: '400', color: '#1BA098'}}>Announcements</h4> 
+      {announcementData.length > 0 ? announcementData.map(a => (
+          <div key={a.id}>
+           <ACard
+              name={a.user.firstName}
+              date={a.date}
+              description={a.message}
+            />
+          </div>
+          )
+        ):"No announcements to display!"}
+    </div>
+    </>
+  )
+}
+
+export default Announcements
